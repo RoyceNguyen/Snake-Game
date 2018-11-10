b49a703ef6b95a4caf602b054d64ad38c8b10fbc
Royce Nguyen <Hoangtu.Nguyen11@stclairconnect.ca>
2018-11-09 21:04:25 -0500

    creating a function for the head to move and a delay timer on the head

--- snake.py
+++ snake.py
@@ -2,14 +2,16 @@
 
 
 import turtle
+import time
 
+delay = 0.1
 # Set up the screen
 
 window = turtle.Screen()
 window.title("Snake Game")
 window.bgcolor("blue")
 window.setup(width=600, height=600)
-window.tracer(0)
+window.tracer(0) #turns off the screen updates
 
 # creating snake head
 head = turtle.Turtle()
@@ -18,8 +20,19 @@ head.shape("circle")
 head.color("red")
 head.penup()
 head.goto(0,0) #start at mid of screen
-head.direction = "stop"
+head.direction = "up"
 
+# Functions
+def move():
+    if head.direction == "up":
+        y = head.ycor()
+        head.sety(y + 20)
+# main game loop
+while True:
+    window.update()
+
+    move()
+    time.sleep(delay)
 
 
 window.mainloop()
