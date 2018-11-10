6e0406ec6fa134e599b3d887228e0885460ebe38
Royce Nguyen <Hoangtu.Nguyen11@stclairconnect.ca>
2018-11-09 20:39:16 -0500

    Git added

---
+++ snake.py
@@ -0,0 +1,19 @@
+
+
+
+import turtle
+
+# Set up the screen
+
+window = turtle.Screen()
+window.title("Snake Game")
+window.bgcolor("blue")
+window.setup(width=600, height=600)
+window.tracer(0)
+
+
+
+
+
+
+window.mainloop()
