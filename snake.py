d029e060cb04a573ca50d14dbf4e557782973014
Royce Nguyen <Hoangtu.Nguyen11@stclairconnect.ca>
2018-11-09 20:50:45 -0500

    Creating the snek head

--- snake.py
+++ snake.py
@@ -11,8 +11,14 @@ window.bgcolor("blue")
 window.setup(width=600, height=600)
 window.tracer(0)
 
-
-
+# creating snake head
+head = turtle.Turtle()
+head.speed(0)
+head.shape("circle")
+head.color("red")
+head.penup()
+head.goto(0,0) #start at mid of screen
+head.direction = "stop"
 
 
 
