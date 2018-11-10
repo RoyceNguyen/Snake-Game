4e302e612b631b583f33dde708832b4f627866f5
Royce Nguyen <Hoangtu.Nguyen11@stclairconnect.ca>
2018-11-09 21:38:08 -0500

    created food object, importing random and creating a function so that if the snake and head collide the food would go to a random palce

--- snake.py
+++ snake.py
@@ -3,13 +3,13 @@
 
 import turtle
 import time
-
+import random
 delay = 0.1
 # Set up the screen
 
 window = turtle.Screen()
 window.title("Snake Game")
-window.bgcolor("blue")
+window.bgcolor("black")
 window.setup(width=600, height=600)
 window.tracer(0) #turns off the screen updates
 
@@ -17,10 +17,20 @@ window.tracer(0) #turns off the screen updates
 head = turtle.Turtle()
 head.speed(0)
 head.shape("circle")
-head.color("red")
+head.color("blue")
 head.penup()
 head.goto(0,0) #start at mid of screen
-head.direction = "up"
+head.direction = "stop"
+
+
+
+# creating fruit
+food = turtle.Turtle()
+food.speed(0)
+food.shape("square")
+food.color("white")
+food.penup()
+food.goto(0,100)
 
 # Functions
 def up():
@@ -45,11 +55,25 @@ def move():
         x = head.xcor()
         head.setx(x + 20)
 
+#keybinding
+window.listen()
+window.onkeypress(up, "w")
+window.onkeypress(down, "s")
+window.onkeypress(left, "a")
+window.onkeypress(right, "d")
+
+
 
 
 # main game loop
 while True:
     window.update()
+
+    if head.distance(food) < 20:
+        #move food to random place
+        x = random.randint(-290,290)
+        y = random.randint(-290,290)
+        food.goto(x, y)
 
     move()
     time.sleep(delay)
