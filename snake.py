bec4cf504fd034a8779d6eb8b06ae9ea47b951de
Royce Nguyen <Hoangtu.Nguyen11@stclairconnect.ca>
2018-11-10 00:50:28 -0500

    creating a score board on top

--- snake.py
+++ snake.py
@@ -34,6 +34,16 @@ food.goto(0,100)
 # creating body
 body = []
 
+# pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.shape("square")
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("Score : 0    High Score: 0", align="center", font=("Courier", 24, "normal"))
+
 # Functions
 def up():
     if head.direction != "down":
